81c150d838f9d903a7de000825df6c2c3a9751a4
Rogerooo <hugo.lopesmatias@gmail.com>
2023-01-13 19:21:05 +0000

    Add project files.

---
+++ HugoMatias/Models/EMKeyModel.cs
@@ -0,0 +1,10 @@
+﻿namespace HugoMatias.Models
+{
+    public class EMKeyModel
+    {
+        public List<int> Numbers { get; set; } = new();
+        public List<int> Stars { get; set; } = new();
+        public double Fee { get; set; }
+        public string Prize { get; set; }
+    }
+}
